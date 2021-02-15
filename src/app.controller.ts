da080a1519f4245c4792e9786e7c21920df085a5
cl3t0 <cl3t0.sh@gmail.com>
2021-02-14 22:04:54 -0300

    feat(initial_structure): first cron job created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  private readonly logger = new Logger(AppController.name);
+
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/phones")
+  getPhones(@Query() query) {
+    console.log(`Finding best phones with budget ${query.price}.`);
+    this.logger.log('oabiawbdiabwi');
+    return [
+      { uid: "9f18d5a8-2baa-4628-93b2-88677e9a290f", price: 1000, name: "Xiaomi Mi A3", picture: "" },
+      { uid: "8ec9def2-40bd-4654-9b2c-9d00e969bb26", price: 800, name: "Samsung M1", picture: "" },
+    ]
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TasksModule } from './tasks/tasks.module';
+import { ScheduleModule } from '@nestjs/schedule';
+
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
