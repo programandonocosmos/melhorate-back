3dcaf892cf88856255f4d6b5e0ad11609c300428
cl3t0 <cl3t0.sh@gmail.com>
2021-02-20 20:21:50 -0300

    fix(initial_structure): some PR related fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,8 +15,7 @@ export class AppController {
 
   @Get("/phones")
   getPhones(@Query() query) {
-    console.log(`Finding best phones with budget ${query.price}.`);
-    this.logger.log('oabiawbdiabwi');
+    this.logger.log(`Finding best phones with budget ${query.price}.`);
     return [
       { uid: "9f18d5a8-2baa-4628-93b2-88677e9a290f", price: 1000, name: "Xiaomi Mi A3", picture: "" },
       { uid: "8ec9def2-40bd-4654-9b2c-9d00e969bb26", price: 800, name: "Samsung M1", picture: "" },
