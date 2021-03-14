4826b020e22cfce1e3d25c7204a4e133636831c3
Davi Suga <daviciencia1@gmail.com>
2021-03-14 19:10:29 -0300

    feat(score_phones): added functions scrap data from geekbench

---
+++ src/scrappper/score-phones/getPhonesMock.ts
@@ -0,0 +1,20 @@
+export default async () => [
+  {
+    name: 'Xiaomi Mi A3',
+    url: '',
+    price: 800,
+    score: null,
+  },
+  {
+    name: 'iPhone 11',
+    url: '',
+    price: 800,
+    score: 100,
+  },
+  {
+    name: 'Motorola Moto G7',
+    url: '',
+    price: 800,
+    score: 80,
+  },
+];
