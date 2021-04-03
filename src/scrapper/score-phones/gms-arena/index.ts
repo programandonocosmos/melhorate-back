0bd5b22c7620bc6cb791e0dc8b38068e3d844e36
Davi Suga <daviciencia1@gmail.com>
2021-04-03 19:30:18 -0300

    refac(feat/score_phones): moved gms arena scrapper to a decent folder name

--- src/scrapper/score-phones/gms-arena/index.ts
+++ src/scrapper/score-phones/gms-arena/index.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
-import type Cheerio from "cheerio";
-const cheerio = require("cheerio");
+import cheerio from "cheerio";
 // const cheerio = require('cheerio') as typeof Cheerio;
-import fs from "fs";
+
+/**
+ * Searchs benchmark scores at GMS Arena
+ *
+ */
 export const getGMSScores = async () => {
   const rawData = await axios.get(
     "https://www.gsmarena.com/benchmark-test.php3"
@@ -12,23 +15,17 @@ export const getGMSScores = async () => {
   const scoreTable = $("#review-body > table.keywords.persist-area > tbody")
     .find("tr")
     .toArray()
-    .map((element, index) => {
-      const phoneName = $(element.firstChild).text();
+    .map((element) => {
+      console.log(element);
+      const phoneName = $(element.next).text();
 
       const [benchA, benchB] = $(element)
         .find("b")
         .toArray()
         .map((element) => $(element).text());
-
-      //   console.log('BENCH A NESSA PORRA ', benchA);
-      //   console.log('BENCH B NESSA PORRA ', benchB);
-
-      //   console.log(phoneName);
       return {
         [phoneName]: { benchA, benchB },
       };
     });
-  //   console.dir(scoreTable);
-
   return { score: scoreTable };
 };
